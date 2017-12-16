aad711bda32ed15fc8902d8f2a5ea53a7447d175
dev-ahmad <ahmadramezhaddad@gmail.com>
2017-12-16 18:55:33 +0200

    integrated DB relationship between topic and blog

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,7 +1,9 @@
 class Blog < ApplicationRecord
 	enum status: {draft: 0 , published: 1 }
 	extend FriendlyId
-  	 friendly_id :title, use: :slugged
+  friendly_id :title, use: :slugged
 
 	validates_presence_of :title , :body #this issue will not allow blog post to be created unless both of these items are filled out
+
+	belongs_to :topic
 end
