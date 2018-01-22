550deadf165a32ad4c7dd3dd1b0b8b3481176dcc
dev-ahmad <ahmadramezhaddad@gmail.com>
2018-01-22 18:11:56 +0200

    implemented concern for managing data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates_presence_of :title , :body , :main_image , :thumb_image
 
 
@@ -9,10 +10,13 @@ class Portfolio < ApplicationRecord
   scope :ruby_on_rails_portfolio_items, ->{where(subtitle: 'Ruby on Rails')}
 
   after_initialize :set_defaults
+  # def set_defaults
+  #   self.main_image ||= "http://placehold.it/900x400"
+  #   self.thumb_image ||= "http://placehold.it/900x400"
+  # end
   def set_defaults
-    self.main_image ||= "http://placehold.it/900x400"
-    self.thumb_image ||= "http://placehold.it/900x400"
+    self.main_image ||=   Placeholder.image_generator(height: '450' , width: '250')
+    self.thumb_image ||=   Placeholder.image_generator(height: '100' , width: '50')
   end
 
-  
 end
